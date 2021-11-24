b357b9459feb2f5f05692eff7988546d8961e240
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-11-24 22:22:52 +0530

    reverse string logic practised

---
+++ javascript/hoisting/index.js
@@ -0,0 +1,4 @@
+console.log(x);
+console.log(y);
+var x = 1;
+let y = 3;
--- javascript/reverse-string/index.js
+++ javascript/reverse-string/index.js
@@ -1,7 +1,28 @@
-const str = "Happy Birthday";
+// const str = "Happy Birthday";
 
-function revStr(string) {
-  return string.split("").reverse().join("");
+// function revStr(string) {
+//   return string.split("").reverse().join("");
+// }
+
+// console.log(revStr(str));
+
+// function reverse(str) {
+//   var string = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     string += str[i];
+//   }
+//   return string;
+// }
+
+// console.log(reverse("tanay sir"));
+
+const str = "i love bangalore";
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.substr(1);
 }
 
-console.log(revStr(str));
+const caps = str.split(" ").map(capitalize);
+console.log(caps);
+const newCps = caps.join(" ");
+console.log(newCps);
